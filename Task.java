a003864b2af7c56537bf6165b428c0f298ffdaee
jayce <jayce.baxter@proton.me>
2025-04-16 02:43:40 -0400

    bug fixing and commenting still have some issues

--- Task.java
+++ Task.java
@@ -6,16 +6,29 @@ package tasklist;
  */
 
 /**
- *
- * @author hezek
+ * @Program Task.java
+ * @Author Hezikiah Cua and Jayce Baxter
+ * @Date April 16th 2025
+ */
+
+/**
+ * Task class
  */
 public class Task {
     
+    // Declaring variables
     private int taskNumber;
     private String description;
     private String deadline;
     private boolean isDone;
 
+    /**
+     * Task constructor
+     * @param taskNumber task number / ID
+     * @param description task description
+     * @param deadline task deadline
+     * @param isDone is the task complete
+     */
     public Task(int taskNumber, String description, String deadline, boolean isDone) {
         this.taskNumber = taskNumber;
         this.description = description;
@@ -24,16 +37,53 @@ public class Task {
     
     }
 
+    /**
+     * Getter for task number / ID
+     * @return taskNumber
+     */
     public int getTaskNumber() { return taskNumber; }
+    
+    /**
+     * Setter for task number / ID
+     * @param taskNumber
+     */
     public void setTaskNumber(int taskNumber) { this.taskNumber = taskNumber; }
 
+    
+    /**
+     * Getter for task description
+     * @return description
+     */
     public String getDescription() { return description; }
+    
+    /**
+     * Setter for task description
+     * @param description
+     */
     public void setDescription(String description) { this.description = description; }
 
+    /**
+     * Getter for task deadline
+     * @return deadline
+     */
     public String getDeadline() { return deadline; }
+    
+    /**
+     * Setter for task deadline
+     * @param deadline
+     */
     public void setDeadline(String deadline) { this.deadline = deadline; }
 
+    /**
+     * Getter for the checkbox determining if the task is completed
+     * @return isDone
+     */
     public boolean isDone() { return isDone; }
+    
+    /**
+     * Setter for the checkbox determining if the task is completed
+     * @param done 
+     */
     public void setDone(boolean done) { isDone = done; }
     
 }
--- TaskList.java
+++ TaskList.java
@@ -1,3 +1,20 @@
+/**
+ * 
+ * KNOWN ERRORS
+ * 
+ * 
+ * HAVE TO hit "new task" before editing existing task, otherwise it won't save to file
+ * 
+ * forward button always goes to end of list
+ * 
+ * no limit on task description characters
+ * 
+ * able to load blank tasks... bug or feature?
+ * 
+ */
+
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -5,16 +22,23 @@
 package tasklist;
 
 /**
- *
- * @author hezek
+ * @Program TaskList.java
+ * @Author Hezikiah Cua and Jayce Baxter
+ * @Date April 16th, 2025
+ */
+
+/**
+ * TaskList class
  */
 public class TaskList {
 
     /**
-     * @param args the command line arguments
+     * Main method
+     * @param args arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        // This was auto generated I don't really get it <3
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new TaskListUI().setVisible(true);
